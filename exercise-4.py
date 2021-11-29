8c7cefc125d238d7a55a84086e455124f3928f57
chetvergs <zhssamira@gmail.com>
2021-11-29 22:28:58 +0300

    exercises 2 and 4 are done

--- exercise-4.py
+++ exercise-4.py
@@ -2,5 +2,24 @@
 # гласные буквы. Для простоты считайте, что все сообщения будут присылаться на латинице. Не забывайте, что гласные буквы
 # могут быть в верхнем регистре
 
+import telebot
 
+bot = telebot.TeleBot("2114922619:AAGRZzL8T5Oi87tTOMAHRrIJqFtefqQYNUs")
+
+letters = ['а', 'у', 'о', 'ы', 'и', 'э', 'я', 'ю', 'ё', 'е']
+@bot.message_handler(commands=['start'])
+def start_message(message):
+	bot.send_message(message.chat.id, 'Hello')
+
+@bot.message_handler(content_types=['text'])
+def text_message(message):
+	count = 0
+
+	for i in message.text:
+		if i in letters:
+			count += 1
+
+	bot.send_message(message.chat.id, count)
+
+bot.polling()
 
