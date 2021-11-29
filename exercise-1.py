93d0695afe88b24ee1275fbbf0f6e1bee0503fe2
chetvergs <zhssamira@gmail.com>
2021-11-29 19:51:53 +0300

    exercise one is done

---
+++ exercise-1.py
@@ -0,0 +1,18 @@
+# Перед вами 10 программ. Ответьте какими типами данных будет обладать переменная x, в конце каждой программы:
+
+# #1 x = str(int(float(5))) # str
+# #2 x = 'aa' == 'AA' # bool
+# #3 x = {i: i**2 for i in range(5)} - # dict
+# #4 x = set(list((5, 6, 7))) # set
+# #5 a = {1: 1, 2: 4, 3: 9} x = a.get(4) # dict
+# #6 x = [].append('j') - # list
+# #7 for i in range(10): if i < 5: x = 'hello' else: x = 5 - # int
+# #8 x = input('Enter and integer: ')  # int
+# #9 a = 5 b = [1, 3, 5, ] x = 'x' a, b, x = x, a, b # list
+# #10 def func(x, y=5): return x + y x = func('Jaguar ', 'hunter') # str
+
+
+
+
+
+
